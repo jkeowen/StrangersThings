ce3288f081adbcbaa92197bfd8070559e53935dc
Justin Keowen <jkeowen@gmail.com>
2023-02-08 11:04:53 -0600

    login style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,19 @@
 import React from "react";
-import { Form } from "react-router-dom";
 
 const Login = () =>{
     
     return(
-        <form>
-            <input type="text" placeholder="username"/>
-            <input type="text" placeholder="password"/>
-            <button>Login</button>
-        </form>
+        <div className="login-page">
+            <h3 className="login-title">Login to Stranger's Things</h3>
+            <form className="login-form">
+                <input className="login-input" type="text" placeholder="username"/>
+                <input className="login-input" type="text" placeholder="password"/>
+                <div className="login-buttons">
+                  <button className="login-submit">Login</button>
+                  <span className="login-register-question">No Account? <button className="login-register">Register</button></span>
+                </div>
+            </form>
+        </div>
     )
 }
 
